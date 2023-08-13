ed045503602e957da011355368d2854977a4ee2e
ahmetturkyilmaz <turkyilmazah@gmail.com>
2023-08-13 21:20:02 +0300

    dockerfile added

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,6 @@ pool.connect()
 
 app.use("/api/health", standardLimiter, healthMetricRoute);
 
-app.get("/api/health-check").status(200);
+app.get("/api/health-check",(req, res) => res.status(200));
 const port = 3201;
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
